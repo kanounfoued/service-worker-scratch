96da39b9dc716d508a1cf416e04c96f71aa3d2e9
kanounfoued <kanoun-foued@outlook.com>
2021-08-03 15:34:17 +0100

    :wrench: install and activate service worker

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// this variable is used for versioning, and the purpose is just to re-create a new service worker.
+// make an update to update the service worker.
+const version = 1;
+
+self.addEventListener("install", onInstall);
+self.addEventListener("activate", onActivate);
+
+main().catch(console.error);
+
+async function main() {
+  console.log(`Service Worker (${version}) is starting ...`);
+}
+
+async function onInstall(e) {
+  console.log(`Service Worker (${version}) installed`);
+  //   kill the old service worker and start the new one immediately.
+  self.skipWaiting();
+}
+
+async function onActivate(e) {
+  // inform the browser to not shutting down somethings, until you do some stuff for activation.
+  // getting resources ...
+  e.waitUntil(handleActivation());
+}
+
+async function handleActivation() {
+  await clients.claim();
+  console.log(`Service Worker (${version}) activated`);
+}
